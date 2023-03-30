3433a38abc3148d571a8158119eff1c192d48121
JuanseHurtadoF <juansebastianhurtadof@gmail.com>
2023-03-30 12:08:54 -0500

    deploy fix

--- routes/client.js
+++ routes/client.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { getProducts, getCustomers, getTransactions, getGeography } from "../controllers/client.js";
+import {
+  getProducts,
+  getCustomers,
+  getTransactions,
+  getGeography,
+} from "../controllers/client.js";
 
 const router = express.Router();
 
@@ -7,8 +12,5 @@ router.get("/products", getProducts);
 router.get("/customers", getCustomers);
 router.get("/transactions", getTransactions);
 router.get("/geography", getGeography);
-
-
-
 
 export default router;
