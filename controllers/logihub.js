10b5fdc7c03afb846d41b67bf2a6919f2da9135e
Juanse Hurtado <31664711+JuanseHurtadoF@users.noreply.github.com>
2023-07-20 13:10:30 -0500

    Update logihub.js

--- controllers/logihub.js
+++ controllers/logihub.js
@@ -25,7 +25,7 @@ export const createContact = async (req, res) => {
   const { email } = req.body;
 
   if (!email) {
-    return res.status(400).json({ message: "Email, Name and Phone are required" });
+    return res.status(400).json({ message: "Email is required" });
   }
 
   try {
